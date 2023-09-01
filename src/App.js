040e6885496a0652b29ad1820b717abb88be2bd3
Soner-Ozciftci <ozciftcisoner@gmail.com>
2023-09-01 15:30:23 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+<div className='calc-wrapper'>
+  <h1>Calculator</h1>
+</div>
+    </div>
+  );
+}
+
+export default App;
