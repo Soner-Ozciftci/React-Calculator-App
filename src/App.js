abd687b5858bdd49ab3408249691651b55802706
Soner-Ozciftci <ozciftcisoner@gmail.com>
2023-09-04 23:31:42 +0200

    input

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import {useState} from 'react';
 import './App.css';
 import Button from './components/Button'
+import Input from './components/Input'
 
 function App() {
   return (
     <div className="App">
 <div className='calc-wrapper'>
+  <Input/>
   <div className='row'>
   <Button symbol="7"/>
   <Button symbol="8"/>
